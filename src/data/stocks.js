f62443095e0a5b80b636ac2f928fea7a512dc632
Ryan Chan <ryalianese@gmail.com>
2019-04-12 21:36:28 -0700

    Adding project for vue tutorial

---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    {id: 1, name: "BMW", price: 110},
+    {id: 2, name: "Google", price: 210},
+    {id: 3, name: "Bilibili", price: 310},
+    {id: 4, name: "Apple", price: 140},
+    {id: 5, name: "Twitter", price: 510}
+]
